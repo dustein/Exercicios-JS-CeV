fe886314073e41e6be89ed625f3bde1c081ce773
dustein <68870425+dustein@users.noreply.github.com>
2021-02-19 21:26:36 -0300

    atualiza exerciccios

---
+++ Tabuada/script.js
@@ -0,0 +1,14 @@
+function calcular() {
+    let numero = document.getElementById('num')
+    let resultado = document.getElementById('res')
+    let n = Number(numero.value)    
+    if (n == 0) {
+        resultado.innerHTML='Nem vou calcular, é tudo ZERO mesmo...'
+    } else {
+        resultado.innerHTML='Calulando...<hr>'
+        for (let cont = 0; cont <= 10; cont ++) {
+            resultado.innerHTML += `${n} x ${cont} = ${n * cont}<br>`
+        }
+        resultado.innerHTML += '<hr>FIM'
+    }
+}
